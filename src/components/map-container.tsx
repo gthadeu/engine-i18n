92c0a0137bc6c051092e058b91b30c03afb4a9fe
Thadeu <thadeu.qodeless@outlook.com>
2024-02-28 14:46:02 -0300

    feat: geo-utils and origin and destination

--- src/components/map-container.tsx
+++ src/components/map-container.tsx
@@ -1,44 +1,51 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { geoUtils } from "../utils/geo-utils";
 
 const MapContainer = () => {
+  // Coordenadas da origem (ML23)
+  const origin = {
+    lat: -19.91299,
+    lng: -43.940933,
+  };
+
+  // Coordenadas dos destinos (por exemplo, três pontos de entrega)
+  const destinations = [
+    { lat: -19.9131, lng: -43.9409 },
+    { lat: -19.91305, lng: -43.94095 },
+    { lat: -19.91295, lng: -43.94088 },
+    { lat: -23.5489, lng: -46.6388 },
+  ];
+
   const mapStyles = {
     height: "50vh",
     width: "100%",
   };
 
-  const defaultCenter = {
-    lat: -19.912998,
-    lng: -43.940933,
-  };
-
-  const m1 = {
-    lat: -19.9131,
-    lng: -43.9409,
-  };
-
-  const m2 = {
-    lat: -19.91305,
-    lng: -43.94095,
-  };
-
-  const m3 = {
-    lat: -19.91295,
-    lng: -43.94088,
-  };
-
-  const m4 = {
-    lat: -19.91295,
-    lng: -43.94097,
-  };
-
   return (
     <LoadScript googleMapsApiKey="AIzaSyD_Ox8zpx-oKaKmgY-5EwbJbQKbuOE7LK0">
-      <GoogleMap mapContainerStyle={mapStyles} zoom={8} center={defaultCenter}>
-        <Marker position={m1} />
-        <Marker position={m2} />
-        <Marker position={m3} />
-        <Marker position={m4} />
-        {/* Mais marcadores podem ser adicionados aqui com diferentes posições */}
+      {/* TRANSFORMAR EM UM CARD */}
+      <div>
+        Distância entre origem e destino:{" "}
+        {geoUtils(
+          origin.lat,
+          origin.lng,
+          destinations[3].lat,
+          destinations[3].lng
+        )}{" "}
+        km
+      </div>
+      <GoogleMap mapContainerStyle={mapStyles} zoom={15} center={origin}>
+        {/* origem */}
+        <Marker position={origin} label={"ML23"} />
+        <Marker position={origin} label={"ML23"} />
+        {/* destinos */}
+        {destinations.map((destination, index) => (
+          <Marker
+            key={index}
+            position={destination}
+            label={`Destino ${index + 1}`}
+          />
+        ))}
       </GoogleMap>
     </LoadScript>
   );
